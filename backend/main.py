43cf15cf0d1ff643815ba48f9b19d8a531f521dd
Zendelll <zendelll@ya.ru>
2024-09-08 01:58:12 +0200

    add CORS Middleware for frontend

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,19 @@ import os
 from fastapi import FastAPI
 from app.routers import entities
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
-
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(entities.router)
 
